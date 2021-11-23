942afb6ea964814311b8fbedc28c8cbf4f94219c
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-23 15:10:12 +0530

    START :- Displayed Welcome Message.

---
+++ Linked_List.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz.linkedList;
+
+public class Linked_List {
+	public static void main(String[] args) {
+		System.out.println("-----------WELCOME TO LINKED-LIST------------\n");
+	}
+}
