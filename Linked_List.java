6d2f15c70babacbcd0dcf09372639cae19ca56a6
saitarun135 <nikhilnaidu473@gmail.com>
2021-11-23 15:14:08 +0530

    UC-1 :- Created simple linked-list of 56,30,70.

--- Linked_List.java
+++ Linked_List.java
@@ -1,7 +1,34 @@
 package com.bridgeLabz.linkedList;
 
 public class Linked_List {
-	public static void main(String[] args) {
-		System.out.println("-----------WELCOME TO LINKED-LIST------------\n");
+	Node head = null;  // initially we are assigning head as null.
+	
+	/** printing linked list **/
+	
+	public <T> void print() {
+		Node currentNode = head;
+		System.out.println("Linked-List :- ");           // head
+		while(currentNode != null) {                    // 56|next --> 30|next --> 70|next --> null 
+			System.out.print(currentNode.key + " --> ");
+			currentNode = currentNode.next;         
+		}
+		System.out.print("null");
 	}
+	
+	public static void main(String[] args) {
+		/** Displayed welcome message **/
+		System.out.println("-----------WELCOME TO LINKED LIST-------------");
+		
+		/** creating object of Linked_List class **/
+		Linked_List linkedList = new Linked_List();   
+		
+		linkedList.head = new Node(56);                  /** diagram representation **/
+		Node node2 = new Node(30);          //   linkedList.head
+		Node node3 = new Node(70);          //        |
+											//		  V
+		linkedList.head.next = node2;       //      56 | next --> 30 | next --> 70 | next --> null
+		node2.next = node3;									
+			
+		linkedList.print();  //printing linkedlist keys/elements/data.
+	}	
 }
---
+++ Node.java
@@ -0,0 +1,29 @@
+package com.bridgeLabz.linkedList;
+
+public class Node<T> {
+	T key;            // key/Data with Generic type.
+	Node next = null;      // initially next assigned as null.
+	
+	/** Constructore **/
+	public Node(T key) {
+		this.key = key;
+		this.next = next;
+	}
+	
+	/** Getters and Setters **/
+	public T getKey() {
+		return key;
+	}
+
+	public void setKey(T key) {
+		this.key = key;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
